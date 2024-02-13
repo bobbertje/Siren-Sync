99b7d685f383534d6b6ad37e1442d4cb9a1e8a46
bobbertje <149894822+bobbertje@users.noreply.github.com>
2024-02-13 13:50:20 +0100

    Added The files.

---
+++ Siren-Sync/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Bobbert'
+description 'Nederlandse Sirenes'
+version '1.0.0'
+
+files { 
+	"dlc_wmsirens/sirenpack_one.awc",
+	"data/wmsirens_sounds.dat54.nametable",
+	"data/wmsirens_sounds.dat54.rel"
+}
+
+data_file "AUDIO_WAVEPACK" "dlc_wmsirens"
+data_file "AUDIO_SOUNDDATA" "data/wmsirens_sounds.dat"
